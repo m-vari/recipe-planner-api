37ad27a174c4cadeab5c35b22d5e7e5c569a68c9
m-vari <mvari@rocketmail.com>
2026-02-25 16:54:41 +0000

    Creazione struttura applicazione

---
+++ recipe-planner/src/test/java/com/mvari/recipe_planner/RecipePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvari.recipe_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
